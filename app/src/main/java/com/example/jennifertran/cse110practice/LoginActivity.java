c35dc1715a6632fe695fb580cfab0f8d701722e3
Jennifer T <jet054@ucsd.edu>
2015-10-21 15:34:33 -0700

    Starting making the login page. XML file is working correctly but needs more work on where the page directs.

---
+++ app/src/main/java/com/example/jennifertran/cse110practice/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.example.jennifertran.cse110practice;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+
+
+
+    public class LoginActivity extends AppCompatActivity {
+
+        private static final String LOGIN_FILE = "LOGIN_FILE";
+
+        private static final String LOGGED_IN = "LOGGED_IN";
+
+        private SharedPreferences mLoginPreferences;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            // boilerplate...
+            super.onCreate(savedInstanceState);
+
+            mLoginPreferences = getSharedPreferences(LOGIN_FILE, MODE_PRIVATE);
+
+            if (mLoginPreferences.getBoolean(LOGGED_IN, false)) {
+                Intent dashboardIntent = new Intent(this, MainActivity.class);
+                startActivity(dashboardIntent);
+
+                finish();
+            } else {
+                setContentView(R.layout.activity_login);
+
+                // set listener of login button to call login() on press
+                findViewById(R.id.login_page_login_button).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        login();
+                    }
+                });
+            }
+
+        }
+
+        private void login() {
+            // ...
+            mLoginPreferences.edit()
+                    .putBoolean(LOGGED_IN, true)
+                    .apply();
+
+
+            fetchServerSideData();
+
+
+
+            finish();
+
+        }
+
+        private void fetchServerSideData() {
+        }
+
+    }
+
+
