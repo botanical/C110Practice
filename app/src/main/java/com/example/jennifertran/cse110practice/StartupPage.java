4690b1330db727ca9085e94bc3843e63f3de97f0
Tina Ke <tinake08@gmail.com>
2015-10-23 15:54:10 -0700

    actual StartupPage files

---
+++ app/src/main/java/com/example/jennifertran/cse110practice/StartupPage.java
@@ -0,0 +1,13 @@
+package com.example.jennifertran.cse110practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartupPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_page);
+    }
+}
