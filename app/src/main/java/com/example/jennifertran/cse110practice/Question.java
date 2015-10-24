372bdfb3152c388c75f83069c2da97dfece3c5ee
Jennifer T <jet054@ucsd.edu>
2015-10-23 19:14:54 -0700

    Made a really ugly quiz and result activity page using SQLite

---
+++ app/src/main/java/com/example/jennifertran/cse110practice/Question.java
@@ -0,0 +1,68 @@
+package com.example.jennifertran.cse110practice;
+
+
+public class Question {
+    private int ID;
+    private String QUESTION;
+    private String OPTA;
+    private String OPTB;
+    private String OPTC;
+    private String ANSWER;
+    public Question()
+    {
+        ID=0;
+        QUESTION="";
+        OPTA="";
+        OPTB="";
+        OPTC="";
+        ANSWER="";
+    }
+    public Question(String question, String A, String B, String C,
+                    String answer) {
+
+        QUESTION = question;
+        OPTA = A;
+        OPTB = B;
+        OPTC = C;
+        ANSWER = answer;
+    }
+    public int getID()
+    {
+        return ID;
+    }
+    public String getQUESTION() {
+        return QUESTION;
+    }
+    public String getOPTA() {
+        return OPTA;
+    }
+    public String getOPTB() {
+        return OPTB;
+    }
+    public String getOPTC() {
+        return OPTC;
+    }
+    public String getANSWER() {
+        return ANSWER;
+    }
+    public void setID(int id)
+    {
+        ID=id;
+    }
+    public void setQUESTION(String question) {
+        QUESTION = question;
+    }
+    public void setOPTA(String A) {
+        OPTA = A;
+    }
+    public void setOPTB(String B) {
+        OPTB = B;
+    }
+    public void setOPTC(String C) {
+        OPTC = C;
+    }
+    public void setANSWER(String answer) {
+        ANSWER = answer;
+    }
+
+}
--- app/src/main/java/com/example/jennifertran/cse110practice/QuizActivity.java
+++ app/src/main/java/com/example/jennifertran/cse110practice/QuizActivity.java
@@ -1,7 +1,91 @@
 package com.example.jennifertran.cse110practice;
 
-/**
- * Created by JenniferTran on 10/23/15.
- */
-public class QuizActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class QuizActivity extends Activity {
+
+    List<Question> question_list;
+    int score=0;
+    int question_id=0;
+    Question current_question;
+    TextView textQuestion;
+    RadioButton rda, rdb, rdc;
+    Button next_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        // Set up the database
+        DbHelper db = new DbHelper(this);
+
+        question_list = db.getAllQuestions();
+        current_question = question_list.get(question_id);
+
+        // Set up question on page
+        textQuestion = (TextView)findViewById(R.id.textView1);
+        rda = (RadioButton)findViewById(R.id.radio0);
+        rdb = (RadioButton)findViewById(R.id.radio1);
+        rdc = (RadioButton)findViewById(R.id.radio2);
+        next_button = (Button)findViewById(R.id.button1);
+
+        // Set the question son the page
+        setQuestionView();
+
+        // Call listener to check for next page request
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup grp = (RadioGroup)findViewById(R.id.radioGroup1);
+
+                // Save the user's answer
+                RadioButton answer = (RadioButton)findViewById(grp.getCheckedRadioButtonId());
+                Log.d("your answer", current_question.getANSWER() + " " + answer.getText());
+
+                if(current_question.getANSWER().equals(answer.getText()))
+                {
+                    score++;
+                    Log.d("score", "Your score" + score);
+                }
+
+                if(question_id<5){
+                    current_question = question_list.get(question_id);
+                    setQuestionView();
+                }else{
+                    Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
+                    Bundle b = new Bundle();
+                    b.putInt("score", score); //Your score
+                    intent.putExtras(b); //Put your score to your next Intent
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_quiz, menu);
+        return true;
+    }
+    private void setQuestionView()
+    {
+        textQuestion.setText(current_question.getQUESTION());
+        rda.setText(current_question.getOPTA());
+        rdb.setText(current_question.getOPTB());
+        rdc.setText(current_question.getOPTC());
+        question_id++;
+    }
 }
