4b0a71077b61e169480e734d3d417f85c936ee0a
etha7 <ethandeson@gmail.com>
2015-11-15 12:56:39 -0800

    Fixed bug where app crashes on login. Fixed bug where app crashes immediately on quiz start.
    The app still crashes if you try to go to question 3 on the math quiz, because there are
    only 2 questions.
    
    ONLY THE MATH QUIZ WORKS
    and even then you can't submit to results page and previous answers aren't saved
    
    Conflicts:
    	app/src/main/java/com/example/jennifertran/cse110practice/QuizActivity.java
    	app/src/main/res/layout/activity_quiz.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/jennifertran/cse110practice/DbHelperQuiz.java
+++ app/src/main/java/com/example/jennifertran/cse110practice/DbHelperQuiz.java
@@ -3,20 +3,25 @@ package com.example.jennifertran.cse110practice;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v4.util.Pair;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 
 public class DbHelperQuiz extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     // Database Name
-    private static final String DATABASE_NAME = "triviaQuiz";
+    private static final String DATABASE_NAME = "triviaQuiz.db";
     // tasks table name
-    private static final String TABLE_QUEST = "quest";
+    private String table;
     // tasks Table Columns names
     private static final String KEY_ID = "id";
     private static final String KEY_QUES = "question";
@@ -25,24 +30,91 @@ public class DbHelperQuiz extends SQLiteOpenHelper {
     private static final String KEY_OPTB= "optb"; //option b
     private static final String KEY_OPTC= "optc"; //option c
     private static final String KEY_MARKED = "marked"; //marked answer by user
+    private ArrayList<String> optionCols;
 
     private SQLiteDatabase dbase;
-    public DbHelperQuiz(Context context) {
+    public DbHelperQuiz(Context context, String tableName, ArrayList<String> optionCols) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.table = "`"+tableName+"`"; //need backticks in case table name has spaces
+                                        //SINGLE QUOTES DO NOT WORK, MUST USE BACKTICKS
+        this.optionCols = optionCols;
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
         dbase=db;
-        String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_QUEST + " ( "
+        String colQuery = "";
+        for(int i = 0; i < optionCols.size(); i++)
+        {
+            colQuery += optionCols.get(i)+" TEXT, ";
+        }
+        String sql = "CREATE TABLE IF NOT EXISTS " + table + " ( "
                 + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_QUES
-                + " TEXT, " + KEY_ANSWER+ " TEXT, "+KEY_OPTA +" TEXT, "
-                +KEY_OPTB +" TEXT, "+KEY_OPTC+" TEXT, "+KEY_MARKED+" TEXT)";
+                + " TEXT, " + KEY_ANSWER+ " TEXT, "+colQuery+KEY_MARKED+" TEXT)";
+
+        System.out.println("CREATE TABLE QUERY "+sql);
         db.execSQL(sql);
-        addQuestions();
         //db.close()
         //Sets question id's to start from 0 instead of 1
-        String subtraction = "UPDATE " + TABLE_QUEST +  " SET " + KEY_ID + "=" + KEY_ID + " - 1";
+        String subtraction = "UPDATE " + table +  " SET " + KEY_ID + "=" + KEY_ID + " - 1";
         db.execSQL(subtraction);
+    }
+    /* createSubNav (Map<String, List<String>> headerChildPairs
+     *  headerChildPairs: Contains key value pairs where the key is the header of a set of quizzes
+     *  and the value is a list of that header's children (actual quizzes
+     *
+     * CreateSubNav fills the suBnav database from headerChildPairs in the format :
+     * Row: header child0 child1 ....
+     */
+    public void upgradeQuiz(HashMap<String,Pair<Pair<String,String>, ArrayList<String>>>
+                                    questOpPairs)
+    {
+        //The last child in the list of children for each header is actually that row's index
+
+        System.out.println("QUEST OP PAIRS:" + questOpPairs);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(table, null, null); //Delete entries in old table
+        Iterator<?> keyIt = questOpPairs.keySet().iterator();
+        String currRow;
+        ArrayList<String> currOptions;
+        while(keyIt.hasNext())
+        {
+
+            currRow = (String) keyIt.next(); //current id
+            ContentValues colValuePairs = new ContentValues();
+            colValuePairs.put("id", currRow); //key represents column names
+
+            currOptions =  questOpPairs.get(currRow).second;
+            colValuePairs.put("question", questOpPairs.get(currRow).first.first);
+            colValuePairs.put("answer", questOpPairs.get(currRow).first.second);
+
+            //Iterate until last child. The last child is actually the index of the row.
+            for(int i = 0; i < currOptions.size()-1; i++)
+            {
+                colValuePairs.put(optionCols.get(i),  currOptions.get(i));
+            }
+            String marked = currOptions.get(currOptions.size() - 1);
+            colValuePairs.put("marked", marked);
+            db.insert(table, null, colValuePairs);
+        }
+        //db.close();
+    }
+
+    public void createTable()
+    {
+        System.out.println("CREATE TABLE " + table);
+        String colQuery = "";
+        for(int i = 0; i < optionCols.size(); i++)
+        {
+            colQuery += optionCols.get(i)+" TEXT, ";
+        }
+        String sql = "CREATE TABLE IF NOT EXISTS " + table + " ( "
+                + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_QUES
+                + " TEXT, " + KEY_ANSWER+ " TEXT, "+colQuery+KEY_MARKED+" TEXT)";
+
+        System.out.println("CREATE TABLE QUERY "+sql);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL(sql);
+
     }
     private void addQuestions()
     {
@@ -61,7 +133,7 @@ public class DbHelperQuiz extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
         // Drop older table if existed
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUEST);
+        db.execSQL("DROP TABLE IF EXISTS " + table);
         // Create tables again
         onCreate(db);
     }
@@ -78,12 +150,12 @@ public class DbHelperQuiz extends SQLiteOpenHelper {
         values.put(KEY_MARKED, quest.getMARKED());
         //values.put(KEY_MARKED, quest.getMARKED());
         // Inserting Row
-        dbase.insert(TABLE_QUEST, null, values);
+        dbase.insert(table, null, values);
     }
     public List<Question> getAllQuestions() {
         List<Question> quesList = new ArrayList<>();
         // Select All Query
-        String selectQuery = "SELECT  * FROM " + TABLE_QUEST;
+        String selectQuery = "SELECT  * FROM " + table;
         dbase=this.getReadableDatabase();
         Cursor cursor = dbase.rawQuery(selectQuery, null);
         // looping through all rows and adding to list
@@ -105,10 +177,23 @@ public class DbHelperQuiz extends SQLiteOpenHelper {
         cursor.close();
         return quesList;
     }
+
+
+
+    ArrayList<ArrayList<String>> getQuestions (String tableName)
+    {
+        ArrayList<String> row;
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        for(int i = 0 ; (row = getQuestionAsArray(i)) != null ; i++) {
+            table.add(row);
+        }
+        return table;
+    }
+
     public int rowcount()
     {
         int row;
-        String selectQuery = "SELECT  * FROM " + TABLE_QUEST;
+        String selectQuery = "SELECT  * FROM " + table;
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(selectQuery, null);
         row=cursor.getCount();
@@ -116,17 +201,17 @@ public class DbHelperQuiz extends SQLiteOpenHelper {
         return row;
     }
     public void insertIntoDb(String id, ContentValues value) {
-        int x = dbase.update(TABLE_QUEST, value, KEY_ID+"="+id, null);
+        int x = this.getWritableDatabase().update(table, value, KEY_ID + "=" + id, null);
         Log.d("UPDATED???", String.valueOf(x));
         Log.d("VAL", value.toString());
         Log.d("QID", id);
     }
     public String getQuestionEntry(String colName, int id ) {
         String marked;
-        String selectQuery = "SELECT " + colName + " FROM " + TABLE_QUEST
+        String selectQuery = "SELECT " + colName + " FROM " + table
                 + " WHERE " + KEY_ID + "=" + id;
         Log.d("QUERY", selectQuery);
-        Cursor cursor = dbase.rawQuery(selectQuery, null);
+        Cursor cursor = this.getWritableDatabase().rawQuery(selectQuery, null);
 
         if  (cursor.moveToFirst()) {
             marked = cursor.getString(cursor.getColumnIndex(colName));
@@ -139,5 +224,54 @@ public class DbHelperQuiz extends SQLiteOpenHelper {
             cursor.close();
             return "null";
         }
+    }
+
+
+    /************************* Methods for adding questions to quiz object ***********************/
+
+
+    public ArrayList<Question> getQuestionsAsQuestionArray(){
+        ArrayList<ArrayList<String>> questions = getQuestionTable();
+        ArrayList<Question> quests = new ArrayList<>();
+        for (ArrayList<String> quest : questions) {
+            quests.add(Question.arrayListToQuestion(quest));
+        }
+
+        return quests;
+    }
+    private ArrayList<ArrayList<String>> getQuestionTable() {
+        ArrayList<ArrayList<String>> table = new ArrayList<>();
+        ArrayList<String> row;
+        for (int i = 0; ((row = getQuestionAsArray(i)) != null); i++) {
+            table.add(row);
+        }
+        return table;
+    }
+
+    /*
+     * getQuestion (int id)
+     * getQuestion retrieves the a quiz question from the local db and returns it as an ArrayList
+     * on our remote database.
+     * Parameters: int id - question number of the desired question
+     * Returns: ArrayList<String> - ArrayList representing the SQL row of the desired question
+     *
+     */
+    private ArrayList<String> getQuestionAsArray(int id)
+    {
+        String selectQuery = "SELECT * FROM "+table+" WHERE "+KEY_ID+"="+id;
+        Log.d("QUERY", selectQuery);
+
+        Cursor cursor = this.getWritableDatabase().rawQuery(selectQuery, null);
+
+        if(!cursor.moveToFirst()) //moveToFirst returns false when cursor is empty
+            return null;
+
+        ArrayList<String> row = new ArrayList<>();
+        for(int i = 0; i < cursor.getColumnCount(); i++)
+        {
+            row.add(cursor.getString(i));
+        }
+        return row;
+
     }
 }
