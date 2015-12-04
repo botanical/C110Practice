7bc81697f8969d89e819621468508039c8599ee1
Jennifer Tran <jet054@ucsd.edu>
2015-12-04 01:20:44 -0800

    example of working tests

--- app/src/androidTest/java/com/example/jennifertran/cse110practice/MainActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/example/jennifertran/cse110practice/MainActivityInstrumentationTest.java
@@ -5,30 +5,41 @@ package com.example.jennifertran.cse110practice;
  */
 // MainActivityInstrumentationTest.java
 
+import android.app.Application;
 import android.support.test.rule.ActivityTestRule;
+import android.test.ApplicationTestCase;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 // Tests for MainActivity
-public class MainActivityInstrumentationTest {
+public class MainActivityInstrumentationTest extends ApplicationTestCase<Application> {
+    public MainActivityInstrumentationTest() {
+        super(Application.class);
+    }
 
     // Preferred JUnit 4 mechanism of specifying the activity to be launched before each test
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
+    
     // Looks for an EditText with id = "R.id.etInput"
     // Types the text "Hello" into the EditText
     // Verifies the EditText has text "Hello"
     @Test
     public void validateEditText() {
-        onView(withId(R.id.etInput)).perform(typeText("Hello")).check(matches(withText("Hello")));
-    }
+        onView(withId(R.id.login_page_username))
+                .perform(typeText("a"));
+        onView(withId(R.id.login_page_password))
+                .perform(typeText("a"));
+        onView(withId(R.id.login_page_login_button))
+                .perform(click());    }
 }
